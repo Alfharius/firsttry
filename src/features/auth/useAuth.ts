d8b858a27185809dfbb984913ce716340e5c2eb9
alfharius <11092002nikolai@gmail.com>
2026-05-08 09:20:11 +0300

    Initial

---
+++ src/features/auth/useAuth.ts
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+import { AuthContext } from './AuthContextCore'
+
+export function useAuth() {
+  const context = useContext(AuthContext)
+  if (!context) throw new Error('useAuth должен использоваться внутри AuthProvider')
+  return context
+}
+
